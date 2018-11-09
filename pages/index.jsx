9ffdaa56b0b08e37908b97f8fdbe682185b9e5de
Vickensi <vickensi.delacruz>
2018-11-09 18:33:37 -0500

    adding-file

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+import logo from '../static/logo.svg';
+
+export default class Home extends Component {
+  render() {
+    return  <div>
+    			<h1>Sandor</h1>
+				<img src="https://picsum.photos/200/200/?random" alt="img-random"/>
+    			<img src={logo} alt="logo"/>
+    		</div>
+  }
+}
